0ed175bbaddf3a07af42b0aeb4a1ce5433c8db01
DaneroArama <www.aviciiguetta@gmail.com>
2025-01-12 20:33:37 +0630

    Commit

---
+++ ViewModel/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Management.ViewModel
+{
+    public class HomeViewModel: ViewModelBase
+    {
+    }
+}
